5117ab3dbf2777a3351ff5559803f0b0c90577a5
cja119 <cja119@ic.ac.uk>
2025-04-22 14:54:16 +0100

    Added file structure and icon

---
+++ src/hydr-bo/__init__.py
@@ -0,0 +1,3 @@
+from .core import HydroBO
+
+__all__ = ["HydroBO"]
---
+++ src/hydr-bo/gis/tesselate.py
@@ -0,0 +1,10 @@
+"""
+This module will tessalte a country into hexagons of a given size.
+"""
+
+class Tesselate
+    def __init__(self): -> None 
+
+        pass
+
+    
