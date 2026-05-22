4308f2858f39f802d759d7a2a26f65ecb7ce3056
cja119 <cja119@ic.ac.uk>
2026-05-22 08:54:04 +0100

    Updates

--- scripts/constrained_bo.py
+++ scripts/constrained_bo.py
@@ -308,6 +308,8 @@ def main():
     g, c, s = cfg.general, cfg.constrained_bo, cfg.sobol
 
     configure_jax_threads(cfg.nlp.n_devices, cfg.nlp.blas_threads)
+    from hydro_bo.mpc.dispatcher import ensure_ray  # noqa: E402
+    ensure_ray(num_cpus=cfg.general.num_devices)
     from hydro_bo.opt import ConstrainedBayesopt  # noqa: E402
 
     global _eval_counter, _results_log, _bayesopt_dir, _run_timestamp, _master_seed
